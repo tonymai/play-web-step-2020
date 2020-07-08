f2bb7662e4b8b73b9a0f4852d7ffcb4ce8c46ed5
essencec <45109455+essencec@users.noreply.github.com>
2020-07-08 14:07:59 -0400

    Removed react logo from application (#43)
    * Removed react logo from application
    
    * Create test for cluster titles

--- client/src/App.js
+++ client/src/App.js
@@ -19,10 +19,6 @@ class App extends Component {
   render() {
     return (
       <div className='App'>
-        <div className='App-header'>
-          <h2>Welcome to React</h2>
-          {this.state.text}
-        </div>
         <MusicApps />
         <TopCharts />
         <RecommendedCategories />
--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,26 +1,15 @@
 import React from 'react';
-import { FetchMock } from '@react-mock/fetch';
-import { render, wait }from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from './App';
 
-const renderComponent = () =>
-    render(
-        <FetchMock
-            options={{
-                matcher: '/api/v1/test-servlet', method: 'GET', response: 'Hello world',
-            }}
-        >
-            <App />
-        </FetchMock>
-    );
-
-test('verifies fetch is called', async () => {
-    const { getByText } = renderComponent();
-    await wait(() => expect(getByText("Hello world")).toBeInTheDocument());
-})
-
-test('loads and displays greeting', () => {
+test('loads and displays text from Top Charts', () => {
     const { getByText } = render(<App />);
-    const textElement = getByText('Welcome to React');
+    const textElement = getByText('Top Charts');
     expect(textElement).toBeInTheDocument();
-})
+});
+
+test('loads and displays greeting from Recommended Categories', () => {
+    const { getByText } = render(<App />);
+    const textElement = getByText('Browse recommended categories');
+    expect(textElement).toBeInTheDocument();
+});
