d4ce4f317f04848106da0db7cfe3e70a4c0f1cc7
bibi hassan <bibihassan2000@gmail.com>
2020-07-10 18:07:35 +0000

    fixed errors for it to run

--- src/main/java/com/google/sps/models/App.java
+++ src/main/java/com/google/sps/models/App.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 
 public class App {
     int id;
-    String icon, name, category;
+    String icon, name, category, rated, appDescription;
     double rating, price;
 
     App(int id, String name, String icon, String category, double rating, String rated, double price, String appDescription) {
--- src/main/java/com/google/sps/models/Stream.java
+++ src/main/java/com/google/sps/models/Stream.java
@@ -2,13 +2,13 @@ package com.google.sps.servlets;
 import java.util.ArrayList;
 
 public class Stream {
-    ArrayList<TopChartsCluster> topChartStream = new ArrayList<TopChartsCluster>();
+    ArrayList<TopChartsCluster> stream = new ArrayList<TopChartsCluster>();
     ArrayList<StandardCluster> standardClusterStream = new ArrayList<StandardCluster>();
     
     Stream() {
         TopChartsCluster topCharts = new TopChartsCluster();
-        topChartStream.add(topCharts);
+        stream.add(topCharts);
         StandardCluster standard = new StandardCluster();
-        StandardCluster.add(standard);
+        standardClusterStream.add(standard);
     }
 }
